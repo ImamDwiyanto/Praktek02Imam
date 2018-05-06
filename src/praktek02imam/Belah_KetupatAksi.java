6a1c3e0acda16201c6fab984c2bfd2b9f14202e1
Imam Dwiyanto <imamdwiyanto21@gmail.com>
2018-05-06 20:47:59 +0700

    Mengisi Nilai Atribut

--- src/praktek02imam/Belah_KetupatAksi.java
+++ src/praktek02imam/Belah_KetupatAksi.java
@@ -3,5 +3,7 @@ package praktek02imam;
 public class Belah_KetupatAksi {
     public static void main(String[] args) {
         Belah_Ketupat rl = new Belah_Ketupat();
+        rl.sisi = 5;
+        rl.panjang = 7;
     }
 }
