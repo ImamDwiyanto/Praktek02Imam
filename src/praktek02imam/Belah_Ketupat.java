41936adee9d9c2e687253a71b0e3cfa1fb128af9
Imam Dwiyanto <imamdwiyanto21@gmail.com>
2018-05-06 20:42:43 +0700

    Membuat Attribute dan Behavior

--- src/praktek02imam/Belah_Ketupat.java
+++ src/praktek02imam/Belah_Ketupat.java
@@ -1,4 +1,12 @@
 package praktek02imam;
 public class Belah_Ketupat {
+    double sisi;
+    double panjang;
     
+    void cetakInfo(){
+        System.out.println("======================");
+        System.out.println("Sisi    : "+sisi);
+        System.out.println("Panjang : "+panjang);
+        System.out.println("======================");
+    }
 }
